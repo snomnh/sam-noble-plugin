e0c14563f928d7e0e99f6b29ffc66e2f4c00f5fc
dehartmatt <dehart.matt@gmail.com>
2015-10-16 18:55:15 -0500

    plugin boilerplate created

---
+++ includes/class-sam-noble-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://ld50design.net
+ * @since      1.0.0
+ *
+ * @package    Sam_Noble_Plugin
+ * @subpackage Sam_Noble_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Sam_Noble_Plugin
+ * @subpackage Sam_Noble_Plugin/includes
+ * @author     Matt DeHart <dehart.matt@ou.edu>
+ */
+class Sam_Noble_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-sam-noble-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://ld50design.net
+ * @since      1.0.0
+ *
+ * @package    Sam_Noble_Plugin
+ * @subpackage Sam_Noble_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Sam_Noble_Plugin
+ * @subpackage Sam_Noble_Plugin/includes
+ * @author     Matt DeHart <dehart.matt@ou.edu>
+ */
+class Sam_Noble_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
